c2de994aaa75ab6185737b1d252dd718c35b3741
Lam Duong <91770017+duongkimlam31@users.noreply.github.com>
2022-09-19 21:55:57 -0500

    Update time_conversion.py

--- time_conversion.py
+++ time_conversion.py
@@ -1,3 +1,4 @@
+# a function takes in a 12-hour time as a string and convert it to 24-hour
 def timeConversion(s):
     s = s.split(":")
     if s[0] == "12" and s[2][2] == "A":
@@ -11,3 +12,10 @@ def timeConversion(s):
     else:
         s[2] = s[2][0:2]
         return ":".join(s)
+
+#some examples
+if __name__ == "__main__":
+    print(timeConversion("12:00:00AM"))
+    print(timeConversion("12:00:00PM"))
+    print(timeConversion("09:40:30PM"))
+    print(timeConversion("8:25:59PM"))
